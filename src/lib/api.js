aa233623231015096cf1ac5b2afdc6a0beabf2d8
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-07-02 22:34:41 -0400

    Added a new query to API file that makes a protected call to the api by trying to see posts that arent published... currently returning blank which is good and means we're at step 1 of trying to implement JWT authentication.

--- src/lib/api.js
+++ src/lib/api.js
@@ -12,7 +12,27 @@ export async function getSiteInfo() {
 		})
 	});
 	const { data } = await response.json();
-	// console.log('Response: ' + data);
 	// console.log(data);
+	return data;
+}
+
+// Protected API route
+export async function getDraftPosts() {
+	const response = await fetch(import.meta.env.WORDPRESS_API_URL, {
+		method: 'post',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			query: `{
+					pages(where: {status: DRAFT}) {
+						nodes {
+							title
+						}
+					}
+				}
+				`
+		})
+	});
+	const { data } = await response.json();
+	console.log(data);
 	return data;
 }
