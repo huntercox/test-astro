aaf98ff43afe83dc5c8d7e8ac4c122112852e77e
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-07-02 22:28:35 -0400

    Changed AuthForm filetype to astro to avoid anothe dependency in Vue. Also got my first piece of data coming through via the WPGraphQL endpoint.

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,18 +1,18 @@
 export async function getSiteInfo() {
-	const getSiteInfoRes = await fetch(import.meta.env.WORDPRESS_API_URL, {
+	const response = await fetch(import.meta.env.WORDPRESS_API_URL, {
 		method: 'post',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify({
 			query: `{
 						generalSettings {
 								title
-								url
-								description
 						}
 				}
 				`
 		})
 	});
-	const { data } = await getSiteInfoRes.json();
+	const { data } = await response.json();
+	// console.log('Response: ' + data);
+	// console.log(data);
 	return data;
 }
