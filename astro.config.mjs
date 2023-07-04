a28ebd39f1ce22aa2227363a96a994964e1fd38e
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-07-04 09:51:54 -0400

    Update config for WP Engine Atlas setup.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
-	output: 'static',
-	integrations: [vue()]
+  output: 'static',
+  integrations: [vue()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
