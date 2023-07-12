bf6b3ad3f1a3656fb4ba8a7b01d23f57c16c6ab3
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-07-12 18:00:14 -0400

    Remove other unused dependencies and change the output method to static.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import vue from "@astrojs/vue";
-
-// https://astro.build/config
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
-	output: 'hybrid',
-	integrations: [vue()],
-	adapter: node({
-		mode: "standalone"
-	})
+	output: 'static',
 });
